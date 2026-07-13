631e6652660edbdfc0222fff2ad1726e94d5b61e
howrahsatyam135-brahman <howrahsatyam135@gmail.com>
2026-07-13 07:57:04 +0530

    update Shubh -Aarambh.java

---
+++ Shubh -Aarambh.java
@@ -0,0 +1,9 @@
+class ShubhAarambh
+{
+   public static void main(String Args[])
+    { 
+       System.out.println("Jay Shree Ganesh");
+       System.out.println("Vignh Harta Namo Namah");
+       }
+       }
+     
